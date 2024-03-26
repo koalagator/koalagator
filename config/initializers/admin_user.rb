7fd507b8043622ec897cd2d786f058791fb8d940
Moxvallix <moxvallix@gmail.com>
2024-03-26 13:16:12 +1030

    chore: rescue ActiveRecord::StatementInvalid in admin initializer, to fix CI

--- config/initializers/admin_user.rb
+++ config/initializers/admin_user.rb
@@ -2,11 +2,14 @@ module Calagator
   Rails.application.config.after_initialize do
     next unless Calagator.devise_enabled
     next unless Calagator.admin_username.present? && Calagator.admin_email.present? && Calagator.admin_password.present?
-    next if User.admin.any?
-    User.create(
-      name: Calagator.admin_username, email: Calagator.admin_email,
-      password: Calagator.admin_password, password_confirmation: Calagator.admin_password,
-      admin: true
-    )
+    begin
+      next if User.admin.any?
+      User.create(
+        name: Calagator.admin_username, email: Calagator.admin_email,
+        password: Calagator.admin_password, password_confirmation: Calagator.admin_password,
+        admin: true
+      )
+    rescue ActiveRecord::StatementInvalid
+    end
   end
 end
