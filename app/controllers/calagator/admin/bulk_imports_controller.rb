3250e87a895cfc961bdfd96a6ea132d8e856cc93
Richie Khoo <evolve2k@gmail.com>
2025-04-07 21:25:42 +0930

    WIP: CSV Processing (Still to resolve issue where Title keeps getting cleared on save)

--- app/controllers/calagator/admin/bulk_imports_controller.rb
+++ app/controllers/calagator/admin/bulk_imports_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'csv'
 require "calagator/import_events_csv"
 
 module Calagator
@@ -14,29 +14,29 @@ module Calagator
         # example code via: https://guides.rubyonrails.org/form_helpers.html#uploading-files
 
         uploaded_file = params[:csv_file]
+
         if uploaded_file.present?
+          Rails.logger.info "upload_file present"
           csv_data = CSV.parse(uploaded_file.read, headers: true)
-          csv_data.each do |row|
-            # Process each row of the CSV file
-            # SomeInvoiceModel.create(amount: row['Amount'], status: row['Status'])
-            Rails.logger.info row.inspect
-            # <CSV::Row "id":"po_1KE3FRDSYPMwkcNz9SFKuaYd" "Amount":"96.22" "Created (UTC)":"2022-01-04 02:59" "Arrival Date (UTC)":"2022-01-05 00:00" "Status":"paid">
+          bulk_import = Calagator::BulkImport.new(csv_data)
+          bulk_import.process_csv_file(csv_data)
+        else
+          Rails.logger.info "upload_file not present"
+        end
+
+        respond_to do |format|
+          if bulk_import&.csv_errors.empty?
+            Rails.logger.info "csv_errors is empty"
+            format.html { render :new, notice: "Successfully imported all records." }
+          else
+            Rails.logger.info "csv_errors is not empty"
+            Rails.logger.info "#{bulk_import.csv_errors}"
+            # bulk_import.count_saved_successfully = 0
+            # bulk_import.count_not_saved = 0
+            format.html { render :new, notice: "#{0} records imported successully. #{0} records reported the following errors:
+            #{bulk_import.csv_errors}" }
           end
         end
-        # ...
-
-        # see /lib/import_events_csv.rb for the rest of the code.
-        # Suggest move move /lib/import_events_csv.rb into models that are not connected to active record eg PORO.
-
-        # @curation = Curation.new(curation_params)
-
-        # respond_to do |format|
-        #   if @curation.save
-        #     format.html { redirect_to admin_curations_path, notice: "Successfully created curation." }
-        #   else
-        #     format.html { render :new, status: :unprocessable_entity }
-        #   end
-        # end
       end
     end
   end
